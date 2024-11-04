1a403fbadc0a20a5c86d22e6518bc9bd448f4805
NumaDev <at.psicotrisquel@gmail.com>
2024-11-04 18:28:57 -0300

    Add partials ejs

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const port = process.env.PORT || 3100;
 
+console.clear()
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,13 +15,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
-
-
 app.get('/', (req, res) => {
     res.render('index')
 })
-
 
 app.listen(port, () => {
     console.log(`Abrime => http://localhost:${port}/`);
