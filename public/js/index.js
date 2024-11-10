4d9c213df596910ff29a437516125861c6cc82a0
NumaDev <at.psicotrisquel@gmail.com>
2024-11-10 13:53:21 -0300

    Add scroll transition opacity/visibility to floating menu

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,9 @@
 const message = `ALERTA. Será redireccionado a una app VERCEL que google reconoce como pishing, miré el codigo en github y vera que no quiero saber su contraseña.`
 
-function warning(event){
+function warning(event) {
     event.preventDefault()
     const response = confirm(message);
-    if(response) return  window.open("https://my-user-managment.vercel.app/login.html", "_blank");
+    if (response) return window.open("https://my-user-managment.vercel.app/login.html", "_blank");
     return window.location.reload();
 }
 
@@ -11,17 +11,40 @@ function warning(event){
 document.addEventListener("DOMContentLoaded", () => {
     const body = document.getElementById('body');
     const button = document.getElementById('button-mode');
+
     button.addEventListener("click", () => {
         body.classList.toggle('dark-mode');
-        body.style.backgroundColor = body.style.backgroundColor === 'rgb(27, 24, 73)' 
-        ? '#fff' 
-        : 'rgb(27, 24, 73)';
-        }
-    )    
+        body.style.backgroundColor = body.style.backgroundColor === 'rgb(27, 24, 73)'
+            ? '#fff'
+            : 'rgb(27, 24, 73)';
+    }
+    )
+
+    const floatingMenu = document.getElementById('floating-menu');
+    let scrollTimeout;
+
+
+    window.addEventListener('scroll', () => {
+        console.log("Hola")
+        floatingMenu.style.opacity = '1';
+        floatingMenu.style.visibility = 'visible';
+        floatingMenu.style.transitionDuration = '2s'
+
+        clearTimeout(scrollTimeout);
+
+        scrollTimeout = setTimeout(() =>{
+            floatingMenu.style.opacity = '0';
+            floatingMenu.style.visibility = 'hidden';
+        }, 1000)
+    })
+
+    floatingMenu.addEventListener('mouseenter', () =>{
+        clearTimeout(scrollTimeout)
+        floatingMenu.style.opacity = '1';
+        floatingMenu.style.visibility = 'visible';
+    })
+    floatingMenu.addEventListener('mouseleave', () => {
+        floatingMenu.style.opacity = '0';
+        floatingMenu.style.visibility = 'hidden';
+    });
 });
-
-
-// windows.addEventListener('scroll', ()=>{
-//     const flaotingMenu = document.querySelector('#flaoting-menu');
-//     flaotingMenu
-// })
