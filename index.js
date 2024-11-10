234627966c814f799cd19d8176dc2da258cd571c
NumaDev <at.psicotrisquel@gmail.com>
2024-11-10 12:25:40 -0300

    Fix error deploy

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from 'path';
-import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,8 +10,8 @@ const port = process.env.PORT || 3100;
 console.clear()
 const app = express();
 
-// const dataPath = join(__dirname, 'data', 'data.json');
-const data = JSON.parse(fs.readFileSync('./config/data.json', 'utf-8'));
+// // const dataPath = join(__dirname, 'data', 'data.json');
+// const data = JSON.parse(fs.readFileSync('./config/data.json', 'utf-8'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -20,7 +19,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.render('index', { data: data})
+    res.render('index')
 })
 
 app.listen(port, () => {
